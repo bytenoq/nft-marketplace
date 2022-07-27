cda39c9da015b04a953fe3a91efad98ebf1ee667
Giorgos Kriaras <bytenoq@gmail.com>
2022-07-27 14:13:54 +0300

    configured networks

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,23 @@
 require("@nomicfoundation/hardhat-toolbox");
+require("dotenv").config();
 
-/** @type import('hardhat/config').HardhatUserConfig */
+const {API_KEY, PRIVATE_KEY} = process.env;
+
 module.exports = {
+  networks: {
+    hardhat: {
+      chainId: 1337
+    },
+    mainnet: {
+      chainId: 1,
+      url: `https://mainnet.infura.io/v3/${API_KEY}`,
+      accounts: [`0x${PRIVATE_KEY}`]
+    },
+    goerli: {
+      chainId: 5,
+      url: `https://goerli.infura.io/v3/${API_KEY}`,
+      accounts: [`0x${PRIVATE_KEY}`]
+    }
+  },
   solidity: "0.8.9",
 };
