3b3d1b93e30063eed8e6a8dc391110eadcd3b45e
Giorgos Kriaras <bytenoq@gmail.com>
2022-07-27 14:41:11 +0300

    added new contract

---
+++ contracts/NFTMarketplace.sol
@@ -0,0 +1,8 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.9;
+
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+
+contract NFTMarketplace is ERC721URIStorage {
+    
+}
