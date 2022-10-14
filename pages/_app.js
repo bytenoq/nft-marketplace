7f3c0777b2d994f6d4ba4042e8d0ffbe88dba29f
Giorgos Kriaras <bytenoq@gmail.com>
2022-10-14 12:43:58 +0300

    npm update

--- pages/_app.js
+++ pages/_app.js
@@ -4,23 +4,23 @@ import 'bootstrap/dist/css/bootstrap.css'
 function MyApp({ Component, pageProps }) {
   return (
     <div>
-      <nav className="navbar navbar-expand-lg bg-light">
-        <div className="container-fluid">
-          <a className="navbar-brand" href="/">AegeanSea</a>
-          <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
-            <span className="navbar-toggler-icon"></span>
+      <nav class="navbar navbar-expand-lg bg-light">
+        <div class="container-fluid">
+          <a class="navbar-brand" href="/">AegeanSea</a>
+          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
           </button>
-          <div className="collapse navbar-collapse" id="navbarNavDropdown">
-            <ul className="navbar-nav">
-              <li className="nav-item">
-                <a className="nav-link" href="/mint">Mint</a>
+          <div class="collapse navbar-collapse" id="navbarNavDropdown">
+            <ul class="navbar-nav">
+              <li class="nav-item">
+                <a class="nav-link" href="/mint">Mint</a>
               </li>
-              <li className="nav-item">
-                <a className="nav-link" href="/sell">Sell</a>
+              <li class="nav-item">
+                <a class="nav-link" href="/sell">Sell</a>
               </li>
-              <li className="nav-item">
-                <div className="d-grid gap-2 col-6 mx-auto">
-                  <button className="btn btn-primary" type="button">Profile</button>
+              <li class="nav-item">
+                <div class="d-grid gap-2 col-6 mx-auto">
+                  <button class="btn btn-primary" type="button">Profile</button>
                 </div>
               </li>
             </ul>
