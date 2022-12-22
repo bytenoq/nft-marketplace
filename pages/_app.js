62cd4d1191c70a4ef215e686143987066f32bb20
Giorgos Kriaras <bytenoq@gmail.com>
2022-12-22 21:52:50 +0200

    added current account on navbar

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,29 @@
+import { useState, useEffect } from 'react'
+import { ethers } from 'ethers'
 import '../styles/globals.css'
 import 'bootstrap/dist/css/bootstrap.css'
 
 function MyApp({ Component, pageProps }) {
+
+  const [address, setAddress] = useState([''])
+
+  useEffect(() => {
+    connect()
+  }, [])
+
+  async function connect() {
+    const provider = new ethers.providers.Web3Provider(window.ethereum, "any");
+    let accounts = await provider.send("eth_requestAccounts", []);
+    let account = accounts[0];
+    provider.on('accountsChanged', function (accounts) {
+        account = accounts[0];
+    });
+
+    const signer = provider.getSigner();
+    const address = await signer.getAddress();
+    setAddress(address);
+  }
+
   return (
     <div>
       <nav class="navbar sticky-top navbar-expand-lg navbar-dark bg-black">
@@ -20,6 +42,16 @@ function MyApp({ Component, pageProps }) {
               </li>
               <li class="nav-item">
                 <a class="nav-link" href="/buy">Buy</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="/list">List</a>
+              </li>
+            </ul>
+          </div>
+          <div class="collapse navbar-collapse justify-content-end" id="navbarNavDropdown">
+            <ul class="navbar-nav">
+              <li class="nav-item border">
+                <a class="nav-link" href={"https://etherscan.io/address/"+address}>{address}</a>
               </li>
             </ul>
           </div>
