cfcfaa0cc39fe203cea66fff96e036a66f09e80f
Giorgos Kriaras <bytenoq@gmail.com>
2022-12-24 14:33:26 +0200

    changed hardhat config

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,23 +1,11 @@
 require("@nomicfoundation/hardhat-toolbox");
 require("dotenv").config();
 
-const {ETH_API_KEY, ETH_API_KEY_SECRET} = process.env;
-
 module.exports = {
   networks: {
     hardhat: {
       chainId: 1337
     },
-    mainnet: {
-      chainId: 1,
-      url: `https://mainnet.infura.io/v3/${ETH_API_KEY}`,
-      accounts: [`${ETH_API_KEY_SECRET}`]
-    },
-    goerli: {
-      chainId: 5,
-      url: `https://goerli.infura.io/v3/${ETH_API_KEY}`,
-      accounts: [`${ETH_API_KEY_SECRET}`]
-    }
   },
   solidity: "0.8.9",
 };
